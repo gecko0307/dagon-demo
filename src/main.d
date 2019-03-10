63deeeca7206eb16f32e260b2caf24301db63fb7
Timur Gafarov <gecko0307@gmail.com>
2019-03-10 19:37:27 +0300

    Energy

--- src/main.d
+++ src/main.d
@@ -694,7 +694,7 @@ class TestScene: Scene
         {
             Vector3f pos = fpview.camera.position + fpview.camera.characterMatrix.forward * -2.0f + Vector3f(0, 1, 0);
             Color4f color = lightColors[uniform(0, lightColors.length)];
-            createLightBall(pos, color, 3.0f, lightBallRadius, 5.0f);
+            createLightBall(pos, color, 6.0f, lightBallRadius, 5.0f);
         }
     }
     
